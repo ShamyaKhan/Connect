ae5d208f1d7c50ec315d9bdff0220b6f128f98c2
Shamya Khan <md.shamiulhaque127@gmail.com>
2026-03-15 19:52:16 +0600

    progressed little bit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/react";
+import { CLERK_PUBLISHABLE_KEY } from "./utils/constants.js";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>,
 );

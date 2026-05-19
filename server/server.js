6db55e0a50886d7dbd3d7949381e77e42971d30e
Shamya Khan <md.shamiulhaque127@gmail.com>
2026-05-19 21:53:44 +0600

    cors issue fixed

--- server/server.js
+++ server/server.js
@@ -14,12 +14,7 @@ const messageRouter = require("./routes/messageRoutes");
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(clerkMiddleware());
 

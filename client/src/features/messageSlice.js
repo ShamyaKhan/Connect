ee2c0d7ba460d66fb7dc435992e55c3d5c6b2126
Shamya Khan <md.shamiulhaque127@gmail.com>
2026-05-19 22:04:50 +0600

    fixed message issue

--- client/src/features/messageSlice.js
+++ client/src/features/messageSlice.js
@@ -5,7 +5,7 @@ export const fetchMessages = createAsyncThunk(
   "messages/fetchMessages",
   async ({ token, userId }) => {
     const { data } = await api.post(
-      "/api/messages/get",
+      "/api/message/get",
       { to_user_id: userId },
       { headers: { Authorization: `Bearer ${token}` } },
     );
